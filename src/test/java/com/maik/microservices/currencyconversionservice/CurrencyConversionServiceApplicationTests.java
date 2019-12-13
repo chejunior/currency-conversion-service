9eda6529a4bee33bbc49e16722656c51c1fbd6e0
Maichel Castro <mike.castro.b@gmail.com>
2019-12-12 23:10:26 -0500

    first commit

---
+++ src/test/java/com/maik/microservices/currencyconversionservice/CurrencyConversionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maik.microservices.currencyconversionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyConversionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
